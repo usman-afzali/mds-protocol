b783b09fad0ac0ae906c41efc39cc67e31b77825
Usman Afzali <72333002+usman-afzali@users.noreply.github.com>
2024-10-25 00:33:55 +1300

    draft 1 - docx

--- # Create a data frame with the informati.R
+++ # Create a data frame with the informati.R
@@ -19,3 +19,33 @@ total_responded <- sum(data$Responded)
 cat("Total number of organizations: ", total_organizations, "\n")
 cat("Total number of organizations reached out to: ", total_reached_out, "\n")
 cat("Total number of responses: ", total_responded, "\n")
+
+
+
+
+
+
+#| out-width: 50%
+library(ggplot2)
+
+data <- data.frame(
+  Date = c("Dec-2023", "Feb-2024", "Apr-2024", "Jun-2024"),
+  Responses = c(151, 241, 373, 453)
+)
+
+data$Date <- as.Date(paste("01-", data$Date, sep=""), format="%d-%b-%Y")
+
+ggplot(data, aes(x = Date, y = Responses)) +
+  geom_line(color = "#2c3e50", size = 1) +
+  geom_point(color = "#27ae60", size = 2) +
+  geom_text(aes(label = Responses), vjust = -0.5, color = "#e74c3c", size = 3) +  # Changed color to bright green
+  labs(title = "Response Rate Over Time",
+       x = "Date",
+       y = "Number of Responses") +
+  scale_x_date(date_labels = "%b-%Y", date_breaks = "1 month") +
+  theme_light(base_size = 10) +
+  theme(
+    plot.title = element_text(hjust = 0.5, face = "bold", size = 10, color = "#34495e"),
+    axis.title = element_text(face = "bold", size = 10, color = "#34495e"),
+    axis.text = element_text(color = "#2c3e50")
+  )
